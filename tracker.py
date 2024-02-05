2f52e1fc8b0e1e0f0d3a84c6bad9f57c22ad17eb
dustinkeller <dustin.w.keller@gmail.com>
2024-02-04 22:56:00 -0500

    added starter code for parcelsapp api script

---
+++ tracker.py
@@ -0,0 +1,40 @@
+import sys
+import requests
+import time
+from os import environ
+
+
+api_key = environ.get('API_KEY')
+
+if api_key:
+    trackingUrl = 'https://parcelsapp.com/api/v3/shipments/tracking'
+    try:
+        tracking_num = sys.argv[1]
+    except IndexError as err:
+        print("Enter your tracking number in the command line...")
+        raise err
+    
+    shipment = {'trackingId': tracking_num, 'language': 'en', 'country': 'United States'}
+    response = requests.post(trackingUrl, json={'apiKey': api_key, 'shipments': shipment})
+
+
+    if response.status_code == 200:
+        # Get UUID from response
+        uuid = response.json()['uuid']
+        # Function to check tracking status with UUID
+        def check_tracking_status():
+            response = requests.get(trackingUrl, params={'apiKey': api_key, 'uuid': uuid})
+            if response.status_code == 200:
+                if response.json()['done']:
+                    print('Tracking complete')
+                else:
+                    print('Tracking in progress...')
+                    time.sleep(N) # sleep for N sec
+                    check_tracking_status()
+            else:
+                print(response.text)
+        check_tracking_status()
+    else:
+        print(response.text)
+else:
+    print("No API Key found...")
